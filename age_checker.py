2c3112a806cd9adea93f2f149b2c8322a5c46834
Tobijoshua613 <tobijoshua613@gmail.com>
2026-08-07 23:13:41 +0100

    Add age checker project

---
+++ age_checker.py
@@ -0,0 +1,13 @@
+print("=== Age Checker ===")
+
+name = input("What is your name? ")
+age = int(input("How old are you? "))
+
+print("Hello,", name)
+
+if age >= 18:
+    print("You are an adult.")
+else:
+    print("You are under 18.")
+
+print("Keep learning Python! 🚀")
