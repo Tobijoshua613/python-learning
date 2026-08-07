1e726d4c9c6ca7235861c0068a665d58bc29493c
Tobijoshua613 <tobijoshua613@gmail.com>
2026-08-07 17:02:55 +0100

    Create hello.py

---
+++ hello.py
@@ -0,0 +1,9 @@
+print("Hello, World!")
+
+name = "Joshua Tobi"
+goal = "AI Engineer"
+
+print("My name is", name)
+print("I am learning Python to become an", goal)
+
+print("Every expert was once a beginner.")
