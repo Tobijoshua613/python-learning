b48729d11e14d62943089ec389e5abdf318db2fc
Tobijoshua613 <tobijoshua613@gmail.com>
2026-08-07 17:08:28 +0100

    Create calculator.py

---
+++ calculator.py
@@ -0,0 +1,12 @@
+print("=== My First Python Calculator ===")
+
+num1 = 25
+num2 = 5
+
+print("First Number:", num1)
+print("Second Number:", num2)
+
+print("Addition:", num1 + num2)
+print("Subtraction:", num1 - num2)
+print("Multiplication:", num1 * num2)
+print("Division:", num1 / num2)
